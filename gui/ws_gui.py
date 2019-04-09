ee7866574277fa6ceed283c4d97328c2aad8cb36
hanhiver <13910395157@139.com>
2019-04-09 16:00:48 +0800

    update

--- gui/ws_gui.py
+++ gui/ws_gui.py
@@ -1,5 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+#from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
 from first import *
 
 
@@ -7,27 +12,34 @@ class MyWindow(QMainWindow, Ui_ws_mainWindow):
     def __init__(self, parent=None):
         super(MyWindow, self).__init__(parent)
         self.setupUi(self)
+
+        self.timer_camera = QTimer(self)
+        self.cap = cv2.VideoCapture(0)
+        self.timer_camera.timeout.connect(self.show_pic)
+        self.timer_camera.start(10)
 		
 	# 调用打开文件的dialog. 
 	def slot_openFile():
-
 		filename, tmp = QFileDialog.getOpenFileName(
 			self, '打开文件', '../../wsdata/', '*.avi *.mp4')
 
-		if filename is '':
-			return
-
-		self.capture = cv2.VideoCapture(filename)
+		self.filename = filename
+		
+		#self.cap = cv2.VideoCapture(filename)
 
 	# 显示图像
-	def refreshShow():
+	def show_pic():
 
 		# 根据显示label的大小尺寸缩放图像
 		w = self.label_show.width()
 		h = self.label_show.height()
-		
 
-
+		success, frame = self.cap.read()
+		if success:
+			show = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+			showImage = QImage(show.data, show.shape[1], show.shape[0], QImage.Format_RGB888)
+			self.label_show.setPixmap(QPixmap.fromImage(showImage))
+			self.timer_camera.start(10)
 
 
 if __name__ == '__main__':
