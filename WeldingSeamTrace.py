6ea7d3b1bb2f0c51c184e07c44a00e175e55f264
hanhiver <13910395157@139.com>
2019-07-11 15:37:35 +0800

    update

--- WeldingSeamTrace.py
+++ WeldingSeamTrace.py
@@ -544,8 +544,9 @@ def wsVideoPhase(input, output, local_view = True, arduino = False):
     isOutput = True if output != "" else False
     if isOutput:
         output_res = (700, 600)
-        video_FourCC = cv2.VideoWriter_fourcc(*'mp4v')
+        #video_FourCC = cv2.VideoWriter_fourcc(*'mp4v')
         #video_FourCC = -1
+        video_FourCC = cv2.VideoWriter_fourcc("m", "p", "4", "v")
         out = cv2.VideoWriter(output, video_FourCC, 10, output_res)
         if not out.isOpened():
             print('isOpened(): {}. '.format(out.isOpened))
