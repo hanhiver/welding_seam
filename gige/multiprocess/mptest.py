7b829e8481ff69bd45aaca7c8181e3563490b31a
Dong Han <handongfr@gmail.com>
2019-08-24 10:22:33 +0800

    update

--- gige/multiprocess/mptest.py
+++ gige/multiprocess/mptest.py
@@ -5,7 +5,7 @@ Created on Sat Aug 10 16:12:06 2019
 
 @author: dhan
 """
-import os
+import os, sys
 import multiprocessing
 import numpy as np
 import cv2 
@@ -22,6 +22,10 @@ HEIGHT = 1200
 # Shared device manager between processes. 
 device_manager = None
 cam = None
+
+# Shared vid to read video file or open local cam. 
+vid = None
+
 accum_time = 0
 curr_fps = 0
 prev_time = time.time()
@@ -161,7 +165,68 @@ def get_frame_from_camera(shared_array, shared_value, lock, time_debug = False):
     return True
 
 
-def main():
+def init_videofile(filename):
+    global vid 
+
+    vid = cv2.VideoCapture(filename)
+
+    if not vid.isOpened():
+        raise IOError("Couldn't open webcam or video: {}".format(input))
+
+def get_frame_from_file(shared_array, shared_value, lock, frame_delay = 0.035, time_debug = False):
+    global vid  
+    global accum_time
+    global curr_fps
+    global prev_time
+    
+    curr_time = time.time()
+    exec_time = curr_time - prev_time
+    prev_time = curr_time
+    accum_time = accum_time + exec_time
+    curr_fps = curr_fps + 1
+    if accum_time > 1:
+        accum_time = accum_time - 1
+        shared_value.value = curr_fps
+        curr_fps = 0
+        
+    #print("Get frame: ", os.getpid())
+    #while True:
+    if time_debug:
+        time_stamp = time.time()
+
+    return_value, frame = vid.read()
+
+    if time_debug:
+        print('Get a frame from file:   {:3.3f} ms'.format((time.time()-time_stamp) * 1000))
+        time_stamp = time.time() 
+    
+    if type(frame) != type(None):
+        #print(frame_in_queue.value)
+        lock.acquire()
+        src = np.ctypeslib.as_ctypes(frame.reshape((HEIGHT * WIDTH * 3)))
+        size = ctypes.sizeof(src)
+        ctypes.memmove(shared_array, src, size)
+        lock.release()
+    
+    if time_debug:
+        print('Send the frame out:     {:3.3f} ms'.format((time.time()-time_stamp) * 1000))
+        time_stamp = time.time()
+
+    time.sleep(frame_delay)
+    
+    if time_debug:   
+        print("")
+
+    return True
+
+def close_file():
+    global vid 
+    
+    print("Camera closed. PID: ", os.getpid())
+    return
+
+
+def main1():
     process_lock = multiprocessing.Lock()
     array_temp = np.ones(shape = (HEIGHT * WIDTH * 3), dtype = np.ubyte)
     shared_array = RawArray(ctypes.c_ubyte, array_temp)
@@ -221,8 +286,70 @@ def main():
     print('Oops, something wrong')
     return False
 
+
+def main(filename):
+    process_lock = multiprocessing.Lock()
+    array_temp = np.ones(shape = (HEIGHT * WIDTH * 3), dtype = np.ubyte)
+    shared_array = RawArray(ctypes.c_ubyte, array_temp)
+    shared_value = RawValue(ctypes.c_uint, 0)
+
+    sched_run = schedrun.SchedRun(func = get_frame_from_file, args = (shared_array, shared_value, process_lock, ), 
+                                  init_func = init_videofile, init_args = (filename, ),
+                                  clean_func = close_file, clean_args = {}, 
+                                  interval = 0.0, 
+                                  init_interval = 0.0)
+    
+    cv2.namedWindow("result", cv2.WINDOW_NORMAL)
+    #cv2.resizeWindow("result", 800, 500)
+    #cv2.moveWindow("result", 100, 100)
+
+    accum_time = 0
+    curr_fps = 0
+    prev_time = time.time()
+    show_fps = "Show FPS: ??"
+    gige_fps = "GigE FPS: ??"
+    font_scale = WIDTH//800 + 1
+    
+    while True:        
+        process_lock.acquire()
+        frame = np.array(shared_array, dtype = np.uint8)
+        process_lock.release()
+        frame = frame.reshape((HEIGHT, WIDTH, 3))
+        
+        curr_time = time.time()
+        exec_time = curr_time - prev_time
+        prev_time = curr_time
+        accum_time = accum_time + exec_time
+        curr_fps = curr_fps + 1
+        if accum_time > 1:
+            accum_time = accum_time - 1
+            show_fps = "Show FPS: " + str(curr_fps)
+            curr_fps = 0
+        
+        if shared_value.value > 0:
+            gige_fps = "GigE FPS: " + str(shared_value.value)
+        
+        #fps = gige_fps + " VS " + show_fps
+        cv2.putText(frame, text=gige_fps, org=(30, 80), fontFace=cv2.FONT_HERSHEY_TRIPLEX, 
+                    fontScale=font_scale, color=(0, 0, 255), thickness=2)
+        cv2.putText(frame, text=show_fps, org=(30, 160), fontFace=cv2.FONT_HERSHEY_TRIPLEX, 
+                    fontScale=font_scale, color=(0, 0, 255), thickness=2)
+
+        cv2.imshow('result', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            sched_run.stop()
+            cv2.destroyAllWindows()
+            return True
+
+    # Normally, should not be here. 
+    sched_run.stop()
+    cv2.destroyAllWindows()
+    print('Oops, something wrong')
+    return False
+
+
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
 
 
 
